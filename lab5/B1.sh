243e31d10ba01c11918bb185ffb4f504401b35f0
Ayushi Patel <140989912+Ayushi1229@users.noreply.github.com>
2026-02-16 21:07:09 +0530

    Update B1.sh

--- lab5/B1.sh
+++ lab5/B1.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 #Lab 5 B1. To check that given number is negative or positive
+echo "Enter a number:"
+read n
 
+if [ $n -ge 0 ]; then
+  echo "Positive Number"
+else
+  echo "Negative Number"
+fi
 
