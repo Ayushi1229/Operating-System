1a3c632441844adf302fb0730e3e55620bba2265
Ayushi Patel <140989912+Ayushi1229@users.noreply.github.com>
2026-02-16 21:13:30 +0530

    Update B2.sh

--- lab5/B2.sh
+++ lab5/B2.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 #Lab 5 B2 To check that given number is divisible by 5 or not
+echo "Enter a number:"
+read n
+
+if [ $((n%5)) -eq 0 ]; then
+  echo "Divisible by 5"
+else
+  echo "Not divisible by 5"
+fi
 
