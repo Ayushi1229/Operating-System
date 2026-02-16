99aaff1c3abad4bdd1f00201afb088eeaefea1d1
Ayushi Patel <140989912+Ayushi1229@users.noreply.github.com>
2026-02-16 21:14:17 +0530

    Add age input and voting eligibility check

--- lab5/B3.sh
+++ lab5/B3.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 #Lab 5 B3 To check whether given no is odd or even
+echo "Enter your age:"
+read age
+
+if [ $age -gt 18 ]; then
+  echo "Eligible to Vote"
+else
+  echo "Not Eligible to Vote"
+fi
 
